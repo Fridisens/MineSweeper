e9f5f6d458c14b244c31056974b467dc5daa96cb
Andreas Selguson <andreas.selguson@saabgroup.com>
2023-10-25 10:38:23 +0200

    added initial loop for rerun the game when gameover

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        boolean cont = true;
+        boolean gameOver = false;
+        Scanner sc = new Scanner(System.in);
 
-        //Skapa nytt bräde
 
-        Board b = new Board(16);
-        Board.printBoard(b.board);
 
+        while(cont){
+            while(!gameOver){
+                //Skapa nytt bräde
+                Board b = new Board(16);
+                Board.printBoard(b.board);
+
+                //Spela spelet
+
+                gameOver = b.gameOver();
+            }
+
+            // Fråga om man vill spela igen, eller avsluta spelet
+            System.out.println("Vill du spela igen? (J/N)");
+            String answer = sc.nextLine();
+            if(answer.equals("J")) {
+                gameOver = false;
+                cont = true;
+            }
+            else
+                cont = false;
+
+
+
+        }
         /* Hur man använder metoderna getRowIndex och getColumnIndex:
         Om spelaren t.ex. anger att den vill undersöka ruta c6,
         så kan man skriva i = Board.getRowIndex("c6") för att få fram rätt radindex (2)
